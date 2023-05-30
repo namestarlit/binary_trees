0d0f12dbbce7bd113c5cb61751800738035ede34
Paul John <rainson.work@gmail.com>
2023-05-30 20:30:37 +0300

    Modify header file

--- binary_trees.h
+++ binary_trees.h
@@ -1,5 +1,5 @@
-#ifndef _BINARY_TREES_H_
-#define _BINARY_TREES_H_
+#ifndef __BINARY_TREES_H__
+#define __BINARY_TREES_H__
 
 #include <stddef.h>
 
@@ -11,14 +11,22 @@
  * @left: Pointer to the left child node
  * @right: Pointer to the right child node
  */
-typedef struct binary_tree_s
+struct binary_tree_s
 {
 	int n;
 	struct binary_tree_s *parent;
 	struct binary_tree_s *left;
 	struct binary_tree_s *right;
-} binary_tree_t;
+};
 
+/* ====== TYPEDEF OF BINARY TREES ====== */
+
+typedef struct binary_tree_s binary_tree_t;	/* Binary Tree */
+typedef struct binary_tree_s bst_t;		/* Binary Search Tree */
+typedef struct binary_tree_s avl_t;		/* AVL Tree */
+typedef struct binary_tree_s heap_t;		/* Max Binary Heap */
+
+/* ====== FUNCTION PROTOTYPES ====== */
 void binary_tree_print(const binary_tree_t *);
 
-#endif /* _BINARY_TREES_H_ */
+#endif /* __BINARY_TREES_H__ */
